a7b07a74c6ba68261ff14400f5adc449fb8777c3
AbhishekP-Maplelabs <102032532+AbhishekP-Maplelabs@users.noreply.github.com>
2022-05-30 11:34:03 +0530

    Add files via upload

---
+++ largest_and_smallest_in_tuple.py
@@ -0,0 +1,26 @@
+"""
+    Description: To find the largest and smallest number in the tuple
+"""
+def largest_smallest(number_1):
+    """
+        This function will print the largesr and smallest number in the tuple
+    """
+    LIST=[]
+    for i in range(number_1):
+        LIST.append(input())
+    TUPLE=tuple(LIST)
+    print(TUPLE)
+    LARGEST=TUPLE[0]
+    SMALLEST=TUPLE[0]
+    for i in range(1,len(TUPLE)):
+        if LARGEST<TUPLE[i]:
+            LARGEST=TUPLE[i]
+        if SMALLEST>TUPLE[i]:
+            SMALLEST=TUPLE[i]
+    print(f"The largest item in the tuple is: {LARGEST}")
+    print(f"The smallet item in the tuple is: {SMALLEST}")
+try:
+    NUMBER=int(input("ENter the number of elements: "))
+    largest_smallest(NUMBER)
+except Exception as e:
+    print("Enter valid input")
