5d537c38bf4cb37fa825edc515fe3169092e2ea0
AbhishekP-Maplelabs <102032532+AbhishekP-Maplelabs@users.noreply.github.com>
2022-05-30 17:03:09 +0530

    Add files via upload

---
+++ sub_sets.py
@@ -0,0 +1,29 @@
+"""
+    Description: To find all the possible subsets of distinct integers given in list.
+"""
+class GetSubset:
+    """
+        To get the possible subsets from distinct numbers given by user
+    """
+    def sort_list(self, my_list):
+        """
+            This function will sort the list before creating the subsets.
+        """
+        return self. subset_find([], sorted(my_list))
+    def subset_find(self, curr, my_list):
+        """
+        This function returns all the possible subsets.
+        """
+        if my_list:
+            return self. subset_find(curr, my_list[1:]) + self. subset_find(curr + [my_list[0]], my_list[1:])
+        return [curr]
+try:
+    my_list = []
+    num_elem = int(input("Enter the number of elements in the list: "))
+    for i in range(0,num_elem):
+        elem=int(input())
+        my_list.append(elem)
+    print("Subsets of the list are : ")
+    print(GetSubset().sort_list(my_list))
+except Exception as e:
+    print("Enter valid input")
